ddc0b0f738a756e95b66dda98c02d47e03625171
muralikrishna554 <87712722+muralikrishna554@users.noreply.github.com>
2021-07-21 09:32:01 +0530

    Add files via upload

---
+++ app/router.js
@@ -0,0 +1,13 @@
+import EmberRouter from '@ember/routing/router';
+import config from 'ember-quickstart/config/environment';
+
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
+
+Router.map(function () {
+  this.route('about');
+  this.route('contact', { path: '/getting-in-touch' });
+
+});
